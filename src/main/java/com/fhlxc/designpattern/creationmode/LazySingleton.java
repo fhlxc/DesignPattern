832373c93e512a4f27122bfb7347270340ae072d
fhlxc <z15881614642@163.com>
2020-04-13 22:07:02 +0800

    单例模式

---
+++ src/main/java/com/fhlxc/designpattern/creationmode/LazySingleton.java
@@ -0,0 +1,28 @@
+package com.fhlxc.designpattern.creationmode;
+
+/**
+* @author Xingchao Long
+* @date 2020年4月13日 下午9:40:47
+* @classname LazySingleton
+* @description 
+* 懒汉单例模式，当调用getInstance方法时才会生成
+*/
+
+public class LazySingleton {
+    
+    private static volatile LazySingleton instance = null;
+    
+    private LazySingleton() {
+        //避免类在外部使用默认的构造函数被实例化
+    }
+    
+    public static synchronized LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+        } else {
+            System.out.println("单例对象已经创建！");
+        }
+        return instance;
+    }
+
+}
